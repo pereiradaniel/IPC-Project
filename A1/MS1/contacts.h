d9bb7015253e9868b2704fd097118a0abc8ddc93
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-10-17 11:41:49 -0400

    contacts.h: structs added >> Address, Numbersumbers.

--- A1/MS1/contacts.h
+++ A1/MS1/contacts.h
@@ -1,25 +1,21 @@
-//==============================================
-// Name:           Full name here
-// Student Number: #########
-// Email:          userID@myseneca.ca
-// Section:        XXX
-// Date:           
-//==============================================
-// Assignment:     1
-// Milestone:      1
-//==============================================
-
 // Structure type Name declaration
 struct Name {
-    char firstName[31];
-    char middleInitial[7];
-    char lastName[36];
+	char 	firstName[31];
+	char 	middleInitial[7];
+	char 	lastName[36];
 };
 
 // Structure type Address declaration
-// Place your code here...
+struct Address {
 
+	int 	streetNumber, apartmentNumber;
+	char 	street[41], postalCode[8], city[41];
+
+};
 
 // Structure type Numbers declaration
-// Place your code here...
+struct Numbers {
 
+	char 	cell[11], home[11], business[11];
+
+};
