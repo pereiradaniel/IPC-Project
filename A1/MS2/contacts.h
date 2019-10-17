c5e639a6a50ca1f0cdc098aab253080f45a5389d
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-10-17 11:47:29 -0400

    Copy contacts.h from MS1 to MS2 dir.

---
+++ A1/MS2/contacts.h
@@ -0,0 +1,21 @@
+// Structure type Name declaration
+struct Name {
+	char 	firstName[31];
+	char 	middleInitial[7];
+	char 	lastName[36];
+};
+
+// Structure type Address declaration
+struct Address {
+
+	int 	streetNumber, apartmentNumber;
+	char 	street[41], postalCode[8], city[41];
+
+};
+
+// Structure type Numbers declaration
+struct Numbers {
+
+	char 	cell[11], home[11], business[11];
+
+};
