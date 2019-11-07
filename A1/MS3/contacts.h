cc87e30f50ef6450c341bf89fa709333311c467f
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-11-07 11:06:02 -0500

    Add contacts.h to MS3

---
+++ A1/MS3/contacts.h
@@ -0,0 +1,32 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Date:           
+//==============================================
+// Assignment:     1
+// Milestone:      3
+//==============================================
+
+// Structure type Name declaration
+struct Name {
+	char 	firstName[31];
+	char 	middleInitial[7];
+	char 	lastName[36];
+};
+
+// Structure type Address declaration
+struct Address {
+
+	int 	streetNumber, apartmentNumber;
+	char 	street[41], postalCode[8], city[41];
+
+};
+
+// Structure type Numbers declaration
+struct Numbers {
+
+	char 	cell[11], home[11], business[11];
+
+};
